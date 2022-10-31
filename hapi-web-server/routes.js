4235427d7bd3d08fad4cf4d17dcfd04350043745
lutfi-haslab <work.lutfiikbalmajid@gmail.com>
2022-10-31 11:17:10 +0700

    web services CRUD for notes app

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,65 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
+    },
+  },
+  {
+    method: "POST",
+    path: "/login",
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return h
+        .response(`Welcome ${username}!`)
+        .type("text/plain")
+        .header("X-Custom", "some-value")
+        .code(201);
+    },
+  },
+
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+];
+
+module.exports = routes;
--- notes-app/server.js
+++ notes-app/server.js
@@ -4,11 +4,15 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "localhost",
+    host: 'localhost',
+    "routes": {
+      cors: {
+        origin: ["*"],
+      },
+    }
   });
 
   server.route(routes);
-
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
